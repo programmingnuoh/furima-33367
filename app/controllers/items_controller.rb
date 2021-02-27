592f1f5457c2af579eea4ec313b4a7875f99e5f8
programmingnuoh <myth0725@gmail.com>
2021-02-27 17:12:43 +0900

    ルーティング、コントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    Item.create(item_params)
+    if item.save
+      redirect_to
+    else
+      render
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
