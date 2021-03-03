4bc8d3765713d15a9b08451c82f938874be48723
programmingnuoh <myth0725@gmail.com>
2021-03-03 15:06:40 +0900

    修正3回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   end
 
   belongs_to :user
-  has_one :order_item
+  # has_one :order_item
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
