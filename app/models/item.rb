f303ec015d1eabe4e03e9926aa520ea000a6e5ea
programmingnuoh <myth0725@gmail.com>
2021-03-03 15:39:51 +0900

    rubocop実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,10 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
-    validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A[0-9]+\z/ }
     validates :image
-    with_options numericality:{ other_than: 0 } do
+    with_options numericality: { other_than: 0 } do
       validates :category_id
       validates :sales_status_id
       validates :shipping_fee_status_id
