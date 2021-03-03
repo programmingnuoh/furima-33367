d5ada7e292fb0d2071d3d82d4a4a393a7e6ea847
programmingnuoh <myth0725@gmail.com>
2021-03-03 11:30:30 +0900

    モデルテストの事前準備

---
+++ spec/factories/items.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :item do
+    name {Faker::Games::Pokemon.name}
+    info {Faker::Games::Pokemon.location}
+    category_id {rand(1..9)}
+    sales_status_id {rand(1..2)}
+    prefecture_id {rand(1..47)}
+    scheduled_delivery_id {rand(1..3)}
+    price {rand(300..9999999)}
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe '商品登録機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+    context '商品登録ができる時' do
+      it '全ての項目を入力していると商品を出品できる' do
+        
+      end
+    end
+
+    context '商品登録ができない時' do
+      it 'imageを一枚つけないと商品登録ができない' do
+        
+      end
+
+      it '商品名が空だと登録ができない' do
+        
+      end
+
+      it '商品説明が空だと登録ができない' do
+        
+      end
+
+      it 'カテゴリーの情報が空だと登録ができない' do
+        
+      end
+
+      it '商品の状態の情報が空だと登録ができない' do
+        
+      end
+
+      it '配送料の負担についての情報が空だと登録ができない' do
+        
+      end
+
+      it '配送元の情報が空だと登録ができない' do
+        
+      end
+
+      it '配送予定日数の情報が空だと登録ができない' do
+        
+      end
+
+      it '価格の情報が空だと登録ができない' do
+        
+      end
+
+      it '価格の範囲が¥300~¥9,999,999外だと登録ができない' do
+        
+      end
+
+      it '販売価格は半角数字以外だと登録ができない' do
+        
+      end
+    end
+  end
+end
