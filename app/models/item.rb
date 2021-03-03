120f9515b29b0a875401e0eb2c4bf4925ed2b659
programmingnuoh <myth0725@gmail.com>
2021-03-03 12:32:04 +0900

    priceバリデーション追記2回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :scheduled_delivery_id
-    validates :price
+    validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/ }
   end
   validates :image, presence: true
 
