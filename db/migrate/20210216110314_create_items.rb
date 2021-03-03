36b784e7b48785d3fd397fc475d79d2cb945f120
programmingnuoh <myth0725@gmail.com>
2021-03-03 19:24:26 +0900

    Revert "本番環境でのエラーのための修正"
    This reverts commit 6ecff3fc85d4bb498011e492f7f2bd84120cfd7d.

--- db/migrate/20210216110314_create_items.rb
+++ db/migrate/20210216110314_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+
       t.string :name, null: false
       t.text :info, null: false
       t.integer :category_id, null: false
@@ -11,6 +12,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :price, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
+
+      
     end
   end
 end
