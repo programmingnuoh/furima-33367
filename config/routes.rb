18ee8d1abc6a64b84ef086e1e885c7695afcc6d5
programmingnuoh <myth0725@gmail.com>
2021-03-22 22:35:28 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :delivers, only: [:index]
+  resources :items do
+    resources :delivers, only: [:index, :create]
+    resources :order_items, only: [:index]
+  end
 end
