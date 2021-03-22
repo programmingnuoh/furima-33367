edab97ee0da1bc252478b96a5469fac553775a66
programmingnuoh <myth0725@gmail.com>
2021-03-22 22:36:35 +0900

    Deliverコントローラー設定

--- app/controllers/delivers_controller.rb
+++ app/controllers/delivers_controller.rb
@@ -1,11 +1,32 @@
 class DeliversController < ApplicationController
 
   def index
-    @deliver = Deliver.new(deliver_params)
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end  
   end
 
   private
-  def deliver_params
-    params.require(:deliver).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id,item_id: item.id)
+
+  def order_params
+    params.require(:order).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
   end  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: 3000,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
